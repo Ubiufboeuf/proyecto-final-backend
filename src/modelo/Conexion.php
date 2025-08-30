9a135bd2c374bd5445520b916213253eb46df669
MatiasIturralde <matimadfree01@gmail.com>
2025-08-30 17:46:32 -0300

    feat: crear conexión a la bd

---
+++ src/modelo/Conexion.php
@@ -0,0 +1,15 @@
+<?php
+// modelo/Conexion.php
+function connection() {
+    try {
+        $host = "localhost";
+        $bd = "bdproyectofinal";
+        $usuario = "root";
+        $password = "";
+        $puerto = "3306";
+        $mysqli = new mysqli($host, $usuario, $password, $bd, $puerto);
+        return $mysqli;
+    } catch(Exception $e) {
+        echo $e->getMessage();
+    }
+}
