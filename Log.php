23c8a08cbb22cde67b26efbc63b73c8599b9e0e5
MatiasIturralde <matimadfree01@gmail.com>
2025-08-30 17:44:00 -0300

    feat: implementar archivo para recopilar errores

---
+++ Log.php
@@ -0,0 +1,8 @@
+<?php
+      ini_set('display_errors', 0); // Desactivar la visualización de errores
+      ini_set('display_startup_errors', 0); // Desactivar la visualización de errores de inicio
+
+      ini_set('log_errors', 1); // Activar el registro de errores
+      ini_set('error_log', __DIR__.'/errors.log'); // Especific
+
+?>
